5e26d0a8bfccaa7b055d66ae335caa55d1f9499d
markelcenda <Markel@markelcenda>
2020-04-21 11:08:26 +0200

    ApiEliminarUsuario eginda

---
+++ src/api/ApiEliminarUsuario.java
@@ -0,0 +1,57 @@
+package api;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.dao.ModeloUsuario;
+
+/**
+ * Servlet implementation class ApiEliminarUsuario
+ */
+@WebServlet("/ApiEliminarUsuario")
+public class ApiEliminarUsuario extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ApiEliminarUsuario() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		int id=Integer.parseInt(request.getParameter("id"));
+		
+		try {
+			ModeloUsuario modeloUsuario=new ModeloUsuario();
+			modeloUsuario.delete(id);
+			modeloUsuario.getConexion().close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		response.setHeader("Access-Control-Allow-Origin", "*"); 
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
