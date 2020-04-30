89aa25055a521c5bb172cf7fda78d6cfe26371da
markelcenda <Markel@markelcenda>
2020-04-30 09:55:22 +0200

    DniComparator beherakorra

---
+++ src/modelo/util/DniComparator0.java
@@ -0,0 +1,14 @@
+package modelo.util;
+
+import java.util.Comparator;
+
+import modelo.bean.Usuario;
+
+public class DniComparator0 implements Comparator<Usuario>{
+
+	@Override
+	public int compare(Usuario u1, Usuario u2) {
+		return u1.getDni().compareToIgnoreCase(u2.getDni());
+	}
+
+}
