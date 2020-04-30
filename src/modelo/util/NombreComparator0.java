5f6d0acf7dcc20ad8136bd0635cb65ed7a860e11
markelcenda <Markel@markelcenda>
2020-04-30 10:04:41 +0200

    NombreComparator gorakorra

--- src/modelo/util/NombreComparator0.java
+++ src/modelo/util/NombreComparator0.java
@@ -6,6 +6,7 @@ import modelo.bean.Usuario;
 
 public class NombreComparator0 implements Comparator<Usuario>{
 
+	
 	@Override
 	public int compare(Usuario u1, Usuario u2) {
 		return u1.getNombreApellido().compareToIgnoreCase(u2.getNombreApellido());
